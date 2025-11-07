e4976c7bc0d944b8153586d5ff65c598c8c1fefa
skarlet22 <black2244@inbox.ru>
2025-11-07 14:45:51 +0300

    KTB-02-edits have been made

--- src/main/kotlin/additional/KTB-02-File.kt
+++ src/main/kotlin/additional/KTB-02-File.kt
@@ -1,16 +1,23 @@
 package org.example.additional
 
 import java.io.File
+import java.io.IOException
 
 fun main() {
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
-    val lines = wordsFile.readLines()
 
-    for (line in lines) {
-        println(line)
+    try {
+        wordsFile.createNewFile()
+        wordsFile.writeText("hello привет\n")
+        wordsFile.appendText("dog собака\n")
+        wordsFile.appendText("cat кошка\n")
+        val lines = wordsFile.readLines()
+
+        for (line in lines) {
+            println(line)
+        }
+    } catch (e: IOException) {
+        println("Ошибка при работе с файлом: ${e.message}")
+        e.printStackTrace()
     }
 }
