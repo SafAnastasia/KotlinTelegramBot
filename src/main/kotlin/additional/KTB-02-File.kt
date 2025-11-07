d27d129361d5d6a82ee12764d2a9aa8279e052a4
skarlet22 <black2244@inbox.ru>
2025-11-07 19:25:40 +0300

    KTB-04-word class created

--- src/main/kotlin/additional/KTB-02-File.kt
+++ src/main/kotlin/additional/KTB-02-File.kt
@@ -3,19 +3,29 @@ package org.example.additional
 import java.io.File
 import java.io.IOException
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0
+)
 fun main() {
     val wordsFile: File = File("words.txt")
+    val dictionary = mutableListOf<Word>()
 
     try {
         wordsFile.createNewFile()
-        wordsFile.writeText("hello привет\n")
-        wordsFile.appendText("dog собака\n")
-        wordsFile.appendText("cat кошка\n")
-        val lines = wordsFile.readLines()
+        wordsFile.writeText("hello|привет|0\n")
+        wordsFile.appendText("dog|собака|0\n")
+        wordsFile.appendText("cat|кошка|0\n")
+
+        val lines: List<String> = wordsFile.readLines()
 
         for (line in lines) {
-            println(line)
+            val line = line.split("|")
+            val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt())
+            dictionary.add(word)
         }
+        println(dictionary)
     } catch (e: IOException) {
         println("Ошибка при работе с файлом: ${e.message}")
         e.printStackTrace()
