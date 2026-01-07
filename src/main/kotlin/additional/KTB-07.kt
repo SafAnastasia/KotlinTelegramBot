6c9e9eccdf7f7c99640d29ceb8d36b07c781be94
skarlet22 <black2244@inbox.ru>
2026-01-07 16:51:40 +0300

    KTB-07-edits have been made

--- src/main/kotlin/additional/KTB-07.kt
+++ src/main/kotlin/additional/KTB-07.kt
@@ -4,85 +4,105 @@ import java.io.File
 import java.io.IOException
 
 data class Word(
-    val original: String,
-    val translate: String,
-    var correctAnswersCount: Int = 0
+    val original: String, //слово на английском
+    val translate: String, // его перевод
+    var correctAnswersCount: Int = 0 //количество правильных ответов
 )
 
-fun loadDictionary(): List<Word> {
-    val wordsFile = File("words.txt")
-    val dictionary = mutableListOf<Word>()
+fun loadDictionary(): List<Word> {   // функция для загрузки словаря
+    val wordsFile = File("words.txt") //объект файла в формате txt
+    val dictionary = mutableListOf<Word>() //изменяемый список "словарь"
 
     try {
-        if (!wordsFile.exists()) {
-            wordsFile.createNewFile()
+        if (!wordsFile.exists()) { //если файла нет создается пустой файл, функция exists() используется для проверки
+            wordsFile.createNewFile() // существования файла, функция createNewFile() создает новый файл, если его нет
         }
 
-        if (wordsFile.readText().isBlank()) {
-            wordsFile.writeText("hello|привет|0\n")
-            wordsFile.appendText("dog|собака|0\n")
-            wordsFile.appendText("cat|кошка|0\n")
+        if (wordsFile.readText().isBlank()) { //readText() для чтения всего содержимого файла в виде строки
+            wordsFile.writeText("hello|привет|0\n") //isBlank() проверяет пустая ли строка или состоит из пробелов
+            wordsFile.appendText("dog|собака|0\n") //writeText() записывает текст в файле, полностью перезаписывает
+            wordsFile.appendText("cat|кошка|0\n") //файл, appendText() добавляет текс в конец файла.
         }
 
-        val lines: List<String> = wordsFile.readLines()
+        val lines: List<String> = wordsFile.readLines() // readLines() читает файл и возвращает список строк
 
-        for (line in lines) {
-            if (line.isBlank()) continue
-            val parts = line.split("|")
-            if (parts.size < 3) continue
+        for (line in lines) { // перебираем строки с проверкой на пустые строки и пробелы
+            if (line.isBlank()) continue //пропускаем, если пусто
+            val parts = line.split("|")// делим строки на части функцией split()
+            if (parts.size < 3) continue //если количество частей меньше 3х, пропускаем
 
             val word = Word(original = parts[0], translate = parts[1], correctAnswersCount = parts[2].toInt())
-            dictionary.add(word)
-        }
-    } catch (e: IOException) {
+            dictionary.add(word)//создан объект класса Word из "частей" строк, с преобразованием типа данных toInt()
+        }//добавляем в словарь
+    } catch (e: IOException) {//проверка на ошибку, если файл не записался выводим сообщение
         println("Ошибка при работе с файлом: ${e.message}")
         e.printStackTrace()
     }
-    return dictionary
+    return dictionary // возвращаем собранный словарь
+}
+
+fun saveDictionary(dictionary: List<Word>) {//сохраняем текущий словарь, перезаписывая)
+    val wordsFile = File("words.txt")
+
+    try {
+        val content = buildString {
+            for (word in dictionary) {
+                append("${word.original}|${word.translate}|${word.correctAnswersCount}\n")
+            }
+        }
+
+        wordsFile.writeText(content)
+    } catch (e: IOException) {
+        println("Ошибка при сохранении словаря: ${e.message}")
+    }
 }
 
 fun main() {
-    val dictionary = loadDictionary()
+    val dictionary = loadDictionary() //загружаем словарь
 
-    while (true) {
+    while (true) {//главное меню, работает бесконечно, пока не выберем 0.
         println("1 - Учить слова")
         println("2 - Статистика")
         println("0 - Выход")
 
-        val input = readlnOrNull()?.trim() ?: ""
-        when (input) {
-            "1" -> {
+        val input = readlnOrNull()?.trim() ?: "" // проверка на наличие строки, безопасный вызов, строка есть вызывается
+        when (input) {// trim() - убирает пробелы по краям
+            "1" -> {// Учить слова
                 while (true) {
-                    val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }
-                    if (notLearnedList.isEmpty()) {
-                        println("Все слова выучены!")
-                        break
+                    val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }//список с невыученными словами
+                    if (notLearnedList.isEmpty()) {//создается новый список .filter(), с правильными ответами <3
+                        println("Все слова выучены!")//isEmpty() - проверка на пустоту, если пусто значит все выучено
+                        break //выходим
                     }
 
-                    val questionWords = dictionary.shuffled().take(minOf(4, dictionary.size))
-                    val correctAnswer = questionWords.random()
-
+                    val questionWords = notLearnedList.shuffled().take(minOf(4, notLearnedList.size))
+                    val correctAnswer =
+                        questionWords.random()//варианты ответа (до 4х), перемешиваем список невыученных слов shuffled(),
+                    //берем первые 4 элемента из списка или сколько есть, random()выбирает один случайный элемент
+                    val correctAnswerId =
+                        questionWords.indexOf(correctAnswer) + 1 //индекс правильного ответа для пользователя
                     println("Как переводится слово: ${correctAnswer.original}.")
-                    questionWords.forEachIndexed { index, word ->
-                        println("${index + 1} - ${word.translate}")
+                    questionWords.forEachIndexed { index, word -> //выводим варианты ответа, .forEachIndexed()
+                        println("${index + 1} - ${word.translate}")//перебирает список (индекс, элемент слова)
                     }
-                    println("0 - Выйти в меню")
+                    println("0 - Меню")
 
-                    val answer = readlnOrNull()?.trim() ?: ""
-                    if (answer == "0") break
+                    val userAnswerInput = readlnOrNull()?.trim() ?: ""//проверка ввода ответа
+                    if (userAnswerInput == "0") break
 
-                    val chosenIndex = answer.toIntOrNull()
-                    if (chosenIndex == null || chosenIndex !in 1..questionWords.size) {
+                    val chosenId = userAnswerInput.toIntOrNull()//выбранный вариант по числу, переводим ввод в число
+                    if (chosenId == null || chosenId !in 1..questionWords.size) {
                         println("Введите число от 1 до ${questionWords.size} или 0")
-                        continue
+                        continue//если число это число равно нулю и входит в диапазон 1..кол-во вариантов ответов
+                        //то выводим сообщение
                     }
 
-                    val chosenWord = questionWords[chosenIndex - 1]
-                    if (chosenWord == correctAnswer) {
-                        correctAnswer.correctAnswersCount++
-                        println("Верно! (количество правильных ответов: ${correctAnswer.correctAnswersCount})")
+                    if (chosenId == correctAnswerId) {//сравниваем слово и правильный ответ
+                        println("Правильно!")
+                        correctAnswer.correctAnswersCount++//если совпадает, увеличиваем счетчик
+                        saveDictionary(dictionary)//сохраняем изменения в словаре
                     } else {
-                        println("Неверно. Правильный ответ: ${correctAnswer.translate}.")
+                        println("Неправильно! ${correctAnswer.original} - это ${correctAnswer.translate}.")
                     }
 
                     println()
@@ -90,9 +110,9 @@ fun main() {
             }
 
             "2" -> {
-                val totalCount = dictionary.size
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
-                val percent = if (totalCount > 0) {
+                val totalCount = dictionary.size//
+                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }//выученные слова
+                val percent = if (totalCount > 0) {//процент выученных слов
                     (learnedWords.size.toDouble() / totalCount * 100).toInt()
                 } else 0
 
