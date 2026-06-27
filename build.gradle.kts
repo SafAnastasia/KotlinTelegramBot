5af93026344f011a65eefb313fe270ecd2e50b90
skarlet22 <black2244@inbox.ru>
2026-06-27 18:45:07 +0300

    Add shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.21"
     kotlin("plugin.serialization") version "2.1.21"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.example"
@@ -15,9 +17,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
 }
 
+application {
+    mainClass.set("org.example.additional.TelegramKt")
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(21)
 }
